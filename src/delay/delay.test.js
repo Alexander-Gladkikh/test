d354addccfd71865a585dc6c09a40f0b8148b1ef
Alexander <som17son25@gmail.com>
2023-05-10 19:24:44 +0300

    Initial commit

---
+++ src/delay/delay.test.js
@@ -0,0 +1,11 @@
+const delay = require('./delay')
+
+
+
+describe('delay', () => {
+
+    test('Корректное значение', async () => {
+        const summ = await delay(() => 5+5, 1000)
+       expect(summ).toBe(10)
+    })
+})
